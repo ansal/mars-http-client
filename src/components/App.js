0f00dfd9878fec8650c3baa95bd59be42e4035da
ravi singh <ravics0027@gmail.com>
2019-06-27 13:06:39 +0530

    history

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import listCollectionsComponent from './components/listCollectionsComponent.js'
-
+import ListCollectionsComponent from './listCollectionsComponent.js'
+ import HistoryComponent from './history.js'
 class App extends React.Component{
     render(){
         return(
            
              <div>
                   <h2>hello</h2>
-                  <listCollectionsComponent />
+                  <ListCollectionsComponent />
+                  <HistoryComponent />
              </div>
             
         )
---
+++ src/components/history.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ 
+
+
+class HistoryComponent extends React.Component
+ {
+    
+render()
+{
+    return(
+<p>history compoents</p>
+    );
+}
+
+
+
+ }
+ export default HistoryComponent; 
