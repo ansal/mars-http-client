6c664d11ca8ea2be9b01f3735ef503991ea88444
ravi singh <ravics0027@gmail.com>
2019-07-04 12:43:07 +0530

    added url

--- src/components/history.js
+++ src/components/history.js
@@ -2,145 +2,92 @@ import React from 'react';
 
 class HistoryComponents extends React.Component {
   render() {
+    function Blog(props) {
+      const sidebar = (
+        <ul>
+          {props.posts.map((post) =>
+            <li key={post.id}>
+
+            </li>
+          )}
+        </ul>
+      );
+      const content = props.posts.map((post) =>
+        <div key={post.id}>
+
+          <p>{post.content}</p>
+        </div>
+      );
+      return (
+        <div>
+          {content}
+        </div>
+      );
+    }
+
+    const posts = [
+      { id: 1, content: <a href="#">http://postman/history.js</a> },
+      { id: 2, content: <a href="#">http://postman/history.js</a> },
+      { id: 3, content: <a href="#">http://postman/history.js</a> },
+      { id: 4, content: <a href="#">http://postman/history.js</a> }
+    ];
+
     return (
 
-
-
-      <div className="container">
-
-
-        <div>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">SkyLark</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    New
-        </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Request</a>
-                    <a className="dropdown-item" href="#">Collection</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#"></a>
-                  </div>
-                </li>
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">Import <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Open New</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" href="#">Settings</a>
-                </li>
-              </ul>
-
-            </div>
-
-          </nav>
-        </div>
-
-
-
-
+      < div className="container-fluid">
         <div className="row">
-          <div className="col-4">
-
+          <div className="col-3">
             <div className="card text-center">
               <div className="card-header">
                 <ul className="nav nav-pills card-header-pills">
                   <li className="nav-item">
-                    <a className="nav-link active" href="#">History</a>
+                    <a className="nav-link " href="#">History</a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#">Collection</a>
                   </li>
-
                 </ul>
-
-
               </div>
               <div className="card-body">
 
-                <button style={{ marginRight: 16 }} type="button" className="btn btn-secondary">new collection</button>
-
-
-                <button type="button" className="btn btn-secondary">trash</button>
-
-
-
+                <button type="button" className="btn btn-secondary">new collection</button>
+                <button type="button" className="btn btn-secondary  offset-md-1">trash</button>
               </div>
               <hr />
+
               <div>
 
-                <ul>
-                  <div className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">today</a>
-                  </div>
-                </ul>
-                <ul>
-                  <div>
-                    <a href="http://postman/header">http1</a><br />
-                    <a href="http://postman/header">http2</a><br />
-                    <a href="http://postman/header">http3</a>
-                  </div>
-                </ul>
 
-                <ul>
-                  <div className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">yesterday</a>
+                <div className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">today</a>
+                </div>
 
-                  </div>
-                </ul>
+                <table>
+                  <tr>
+                    <Blog posts={posts} />,
+                </tr>
+                </table>
+                <div className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">yesterday</a>
+
+                </div>
                 <br />
                 <br />
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <a href="#" className="btn btn-success">Go somewhere</a>
                 <br />
                 <br />
                 <br />
 
               </div>
-
             </div>
 
           </div>
 
-
-          <div className="col-7">
-
-
-
-
-
-
-
-
-
-
-
-
-
-          </div>
-
-
-
         </div>
 
       </div>
-
-
-
-
-
     );
   }
 }
-
 
 export default HistoryComponents;
