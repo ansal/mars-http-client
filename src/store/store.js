fa16e53622562d7b82e3aba0d5bed534c2297924
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-07-12 02:48:45 +0530

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,16 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 
-import createCollectionReducer from "./reducers/createCollectionReducer.js";
-import importCollectionsReducer from "./reducers/importCollectionsReducer.js";
-import isUrlLoadingReducer from "./reducers/isUrlLoadingReducer.js";
-import requestResponseReducer from "./reducers/requestResponseReducer.js";
-import saveHistoryReducer from "./reducers/saveHistoryReducer.js";
 import requestReducer from "./reducers/requestReducer.js";
 import paramsReducer from "./reducers/paramsReducer.js";
 import headersReducer from "./reducers/headersReducer.js";
 import bodyReducer from "./reducers/bodyReducer.js";
 
 let reducer = combineReducers({
-    
-    // createCollection: createCollectionReducer,
-    // importCollections: importCollectionsReducer,
-    // isUrlLoading: isUrlLoadingReducer,
     request: requestReducer,
     params: paramsReducer,
     headers: headersReducer,
     body: bodyReducer
-    // requestResponse: requestResponseReducer,
-    // saveHistory: saveHistoryReducer
 })
 
 let store = createStore(reducer);
